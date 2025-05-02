586e6d2e348e895cf44d2b1318a3188fee35c295
Turmaxx <88465473+Turmaxx@users.noreply.github.com>
2025-05-02 10:26:50 +0300

    updated exercise/22_clippy: clippy2.rs
    - added warning for loops over fallibles

--- exercises/22_clippy/clippy2.rs
+++ exercises/22_clippy/clippy2.rs
@@ -2,6 +2,7 @@ fn main() {
     let mut res = 42;
     let option = Some(12);
     // TODO: Fix the Clippy lint.
+    #[warn(for_loops_over_fallibles)]
     if let Some(x) = option {
         res += x;
     }
